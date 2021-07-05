339c4d47b9270e3cb85c67e1a4caa3ad7cbb3382
Carl Wood <cjwood17@googlemail.com>
2021-07-05 21:41:27 +0100

    Remove Image

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 import { getAll, getBySlug } from "../../services/articles";
 import ReactMarkdown from 'react-markdown'
