bbb8ffd5426a8f1639d6a1d3664c8475d82499b7
Carl Wood <cjwood17@googlemail.com>
2021-07-11 16:54:58 +0100

    Loads of head stuff, article styles

---
+++ components/siteHeader.js
@@ -0,0 +1,17 @@
+export default function SiteHeader() {
+  return (
+    <header class="site-header">
+      <a class="logo" href="/">
+        <span>#BLM</span>Resources
+      </a>
+
+      <nav class="nav">
+        <ul class="nav-list">
+          <li>
+            <a href="#">Link</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
